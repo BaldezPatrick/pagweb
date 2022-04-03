9814cc3b13c9a194db3093cb58bb59f66a0e58e5
BaldezPatrick <patrickbaldex@gmail.com>
2022-04-03 15:59:04 -0300

    add welcome internationalization

--- src/Pages/Welcome/welcome.jsx
+++ src/Pages/Welcome/welcome.jsx
@@ -1,12 +1,14 @@
+import { useTranslation } from 'react-i18next';
 import './welcome.css'
 
 const Welcome = () => {
+    const { t } = useTranslation();
     return (
         <>
             <main className="welcome">
                 <section className="welcome_container">
-                    <h1 className="welcome_text">Bem-vindo a tua conta!</h1>
-                    <p className="welcome_subtext">Aqui será seu local de contar os filmes que assistiu!</p>
+                    <h1 className="welcome_text">{t('welcome.welcomeText')}</h1>
+                    <p className="welcome_subtext">{t('welcome.welcomeSubtext')}</p>
                 </section>
 
             </main>
