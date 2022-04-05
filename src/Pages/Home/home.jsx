f55107aee2b3ff4b73c558345c559711be842770
BaldezPatrick <patrickbaldex@gmail.com>
2022-04-04 23:01:33 -0300

    home edition

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,40 +1,20 @@
 import './home.css';
 import Nav from '../../Components/Header/nav';
+import Roupa from '../../Assets/roupas.jpg';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import cosmects from '../../Assets/cosmetics.jpg';
 
 const Home = () => {
     const { t } = useTranslation();
     return (
         <>
             <Nav />
-            <main className="containerHome">
-                <div className="container">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <Link to='#'>{t('home.columns.option1')}</Link>
-                            <figure className="container_img">
-                                <img src={cosmects} alt="" />
-                            </figure>
-                        </div>
-                        <div class="col">
-                            <Link to='#'>{t('home.columns.option2')}</Link>
-                            <figure className="container_img">
-                                <img src={cosmects} alt="" />
-                            </figure>
-                        </div>
-                        <div class="col">
-                            <Link to='#'>{t('home.columns.option3')}</Link>
-                            <figure className="container_img">
-                                <img src={cosmects} alt="" />
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-                <section className="homeTips">
-                    <h1>{t('home.tips.tip')}</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta facilis exercitationem nesciunt sapiente quibusdam debitis natus, dolor consequuntur, totam expedita corrupti reiciendis perspiciatis repudiandae aspernatur eaque, laboriosam ex aperiam quas!</p>
+            <main className="homeContainer">
+                <section className="homeContainerImage">
+                    <img src={Roupa} alt="roupas coloridas penduradas" />
+                </section>
+                <section className="homeContainerHeadline">
+                    {t('home.headline.txt')}
+                    <p>{t('home.headline.subtxt')}</p>
                 </section>
             </main>
         </>
